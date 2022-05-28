7ad013192c0a6f60b0630cdea7401266121ada6c
Captain-Luffy-1 <273629502@qq.com>
2022-05-28 16:47:13 +0800

    Update Topic.java

--- BookAndMovie/src/main/java/com/example/bookandmovie/Entity/Topic.java
+++ BookAndMovie/src/main/java/com/example/bookandmovie/Entity/Topic.java
@@ -1,4 +1,18 @@
 package com.example.bookandmovie.Entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
 public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer topic_id;
+    //topic_id 为自增的主键
+    private Integer focus;//关注数量
+    @Lob
+    private String introduction;//简介
+
+    //接下来展示所属的帖子
 }
